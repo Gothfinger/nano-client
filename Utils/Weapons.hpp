05c85076c1e4990898a533fe46b1ad146286dd2e
Gothfinger <73948251+Gothfinger@users.noreply.github.com>
2024-12-05 19:17:16 +0100

    Add files via upload
    Mostly changes to aimbot and main.cpp

---
+++ Utils/Weapons.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+     
+namespace WeaponIDs {
+    //Weapon Ids
+    //Light
+    int P2020 = 112;
+    int RE45 = 85;
+    int ALTERNATOR = 84;
+    int R301 = 0;
+    int SPITFIRE = 113;
+    int G7 = 95;
+     
+    //Heavy
+    int FLATLINE = 94;
+    int HEMLOCK = 96;
+    int PROWLER = 107;
+    int REPEATER = 118;
+    int RAMPAGE = 6;
+    int CAR = 119;
+     
+    //Energy
+    int HAVOC = 91;
+    int LSTAR = 99;
+    int TRIPLETAKE = 114;
+    int VOLT = 117;
+    int NEMESIS = 120;
+     
+    //Shotgun
+    int MOZAMBIQUE = 102;
+    int EVA8 = 92;
+    int PEACEKEEPER = 109;
+    int MASTIFF = 101;
+     
+    //Sniper
+    int WINGMAN = 115;
+    int LONGBOW = 90;
+    int CHARGE_RIFLE = 87;
+    int SENTINEL = 1;
+     
+    //Legendary
+    int R99 = 111;
+    int DEVOTION = 89;
+    int BOCEK = 2;
+    int KRABER = 98;
+    int KNIFE = 178;
+     
+    //Melee
+    int HANDS = 116;
+};
---
+++ version.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo -n Game version:
+cat /home/user/.var/app/com.valvesoftware.Steam/.steam/steam/steamapps/common/Apex\ Legends/gameversion.txt && 
+echo -n Build version: && 
+cat /home/user/.var/app/com.valvesoftware.Steam/.steam/steam/steamapps/common/Apex\ Legends/build.txt
+
+
